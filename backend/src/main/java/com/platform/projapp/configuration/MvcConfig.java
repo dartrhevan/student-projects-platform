6b2fcbdb4bfb69809d85e1d1029951daf8d8a976
rinya1608 <rinya1608@gmail.com>
2021-10-04 19:22:02 +0500

    configure catch all route for index.html except /api/**

---
+++ backend/src/main/java/com/platform/projapp/configuration/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.platform.projapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Yarullin Renat
+ */
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    private static final String FORWARD = "forward:/";
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{x:[\\w\\-]+}")
+                .setViewName(FORWARD);
+        registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}")
+                .setViewName(FORWARD);
+    }
+}
