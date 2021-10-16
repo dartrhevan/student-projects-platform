ca482b6462fc780f8957dbc62a9f45eb2e2cba1b
rinya1608 <rinya1608@gmail.com>
2021-10-16 20:16:34 +0500

    del exclude

--- backend/src/main/java/com/platform/projapp/ProjappApplication.java
+++ backend/src/main/java/com/platform/projapp/ProjappApplication.java
@@ -2,13 +2,12 @@ package com.platform.projapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class ProjappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjappApplication.class, args);
+    }
 
 }
