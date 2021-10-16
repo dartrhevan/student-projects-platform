ccf816e6f1069bc9bfb58cdd00f2e52a2bb448f0
rinya1608 <rinya1608@gmail.com>
2021-10-16 20:02:25 +0500

    add constants for error

---
+++ backend/src/main/java/com/platform/projapp/error/ErrorConstants.java
@@ -0,0 +1,13 @@
+package com.platform.projapp.error;
+
+/**
+ * @author Yarullin Renat
+ */
+
+public class ErrorConstants {
+    public static ErrorInfo RT_NOT_IN_BD = ErrorInfo.of("RefreshTokenNotInBD","Refresh token отсутствует в базе данных");
+    public static ErrorInfo USERNAME_NOT_FOUND = ErrorInfo.of("UserNameNotFound","Пользователь не найден");
+    public static ErrorInfo USER_NOT_AUTH = ErrorInfo.of("UserNotAuth","Пользователь не авторизован");
+    public static ErrorInfo EMAIL_IS_BUSY = ErrorInfo.of("EmailIsBusy","Пользователь с таким email адресом уже зарегистрирован");
+    public static ErrorInfo LOGIN_IS_BUSY = ErrorInfo.of("LoginIsBusy","Пользователь с таким логином уже зарегистрирован");
+}
