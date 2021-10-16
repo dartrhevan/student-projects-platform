36ce56d2a62881c64754199a952f6ae0dc3e1e13
rinya1608 <rinya1608@gmail.com>
2021-10-16 20:01:27 +0500

    add controller for /refreshtoken and database support

--- backend/src/main/java/com/platform/projapp/controller/AuthController.java
+++ backend/src/main/java/com/platform/projapp/controller/AuthController.java
@@ -1,26 +1,38 @@
 package com.platform.projapp.controller;
 
-import com.platform.projapp.configuration.jwt.JwtUserDetailsService;
 import com.platform.projapp.configuration.jwt.JwtUtils;
-import com.platform.projapp.dto.AuthRequest;
-import com.platform.projapp.dto.JwtResponse;
-import com.platform.projapp.dto.RegisterRequest;
-import com.platform.projapp.enumarate.AccessRole;
-import com.platform.projapp.mock.UserMock;
-import com.platform.projapp.mock.UserRepositoryMock;
+import com.platform.projapp.dto.request.AuthRequest;
+import com.platform.projapp.dto.request.RegisterRequest;
+import com.platform.projapp.dto.request.TokenRefreshRequest;
+import com.platform.projapp.dto.response.GeneralResponse;
+import com.platform.projapp.dto.response.body.JwtResponseBody;
+import com.platform.projapp.dto.response.body.MessageResponseBody;
+import com.platform.projapp.dto.response.body.TokenRefreshResponseBody;
+import com.platform.projapp.error.ErrorConstants;
+import com.platform.projapp.error.ErrorInfo;
+import com.platform.projapp.error.ErrorUtils;
+import com.platform.projapp.model.RefreshToken;
+import com.platform.projapp.model.User;
+import com.platform.projapp.repository.UserRepository;
+import com.platform.projapp.service.RefreshTokenService;
 import com.platform.projapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Set;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Yarullin Renat
@@ -29,39 +41,75 @@ import java.util.Set;
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
-    UserRepositoryMock userRepositoryMock;
+    UserRepository userRepository;
     @Autowired
     UserService userService;
+    @Autowired
+    RefreshTokenService tokenService;
     @Autowired
     JwtUtils jwtUtils;
     @Autowired
     AuthenticationManager authenticationManager;
-    @PostMapping("/signin")
-    public ResponseEntity<?> authUser(@RequestBody AuthRequest authRequest){
 
+    @PostMapping("/signin")
+    public ResponseEntity<?> authUser(@RequestBody AuthRequest authRequest) {
+        GeneralResponse<JwtResponseBody> response = new GeneralResponse<JwtResponseBody>();
         try {
             Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+                    new UsernamePasswordAuthenticationToken(authRequest.getLogin(), authRequest.getPassword()));
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             String jwt = jwtUtils.generateJwtToken(authentication);
 
-            UserMock userMock = (UserMock) authentication.getPrincipal();
+            User user = (User) authentication.getPrincipal();
 
-            return ResponseEntity.ok(new JwtResponse(jwt,userMock));
-        }
-        catch (UsernameNotFoundException e){
-            return ResponseEntity.ok("все плохо");
+            RefreshToken refreshToken = tokenService.createRefreshToken(user);
+
+            response = response.withPayload(new JwtResponseBody(jwt, refreshToken.getToken(), user));
+            return ResponseEntity.ok(response);
+        } catch (UsernameNotFoundException e) {
+            response = response.withErrors(List.of(ErrorConstants.USERNAME_NOT_FOUND));
+            return ResponseEntity.ok(response);
         }
 
     }
+
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest){
-        if (userRepositoryMock.existsByEmail(registerRequest.getEmail())){
-            return ResponseEntity.badRequest().body("Пользователь с таким email адресом уже зарегистрирован");
+    public ResponseEntity<?> registerUser(@RequestBody @Valid RegisterRequest registerRequest, BindingResult bindingResult) {
+        GeneralResponse<MessageResponseBody> response = new GeneralResponse<>();
+
+        List<ErrorInfo> errors = new ArrayList<>();
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            errors.add(ErrorConstants.EMAIL_IS_BUSY);
+        }
+        if (userRepository.existsByLogin(registerRequest.getLogin())) {
+            response = response.withErrors(List.of());
+            errors.add(ErrorConstants.LOGIN_IS_BUSY);
         }
         userService.addUser(registerRequest);
-        return ResponseEntity.badRequest().body("Пользователь зарегистрирован успешно");
+        errors.addAll(ErrorUtils.getErrorInfoFromBindingResult(bindingResult));
+        if (!errors.isEmpty()) response.withErrors(errors);
+        response = response.withPayload(new MessageResponseBody("Пользователь зарегистрирован успешно"));
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping("/refreshtoken")
+    public ResponseEntity<?> refreshToken(@RequestBody TokenRefreshRequest token) {
+        String requestRefreshToken = token.getTokenRefresh();
+        GeneralResponse<TokenRefreshResponseBody> response = new GeneralResponse<>();
+        return Optional.of(tokenService
+                        .findByToken(requestRefreshToken)).map(tokenService::verifyExpiration)
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String accessToken = jwtUtils.generateJwtTokenFromUsername(user.getUsername());
+                    String refreshToken = tokenService.createRefreshToken(user).getToken();
+                    response.withPayload(new TokenRefreshResponseBody(accessToken, refreshToken));
+                    return ResponseEntity.ok(response);
+                }).orElse(ResponseEntity
+                        .badRequest()
+                        .body(new GeneralResponse<TokenRefreshResponseBody>()
+                                .withErrors(List.of(ErrorConstants.RT_NOT_IN_BD)))
+                );
     }
 }
