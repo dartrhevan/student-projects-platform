8c4c6eb219d8b03e2962cc28a09fbe39ee8986aa
rinya1608 <rinya1608@gmail.com>
2021-10-16 20:02:44 +0500

    add dto for error

---
+++ backend/src/main/java/com/platform/projapp/error/ErrorInfo.java
@@ -0,0 +1,15 @@
+package com.platform.projapp.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Yarullin Renat
+ */
+
+@Data
+@AllArgsConstructor(staticName = "of")
+public class ErrorInfo {
+    private String code;
+    private String message;
+}
