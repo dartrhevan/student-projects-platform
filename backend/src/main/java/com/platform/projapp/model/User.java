62c963dc1c421dbf7e20da4ddfc1260e104e7eff
rinya1608 <rinya1608@gmail.com>
2021-11-17 12:44:49 +0500

    change type of field roles on ProjectRole and add field messenger

--- backend/src/main/java/com/platform/projapp/model/User.java
+++ backend/src/main/java/com/platform/projapp/model/User.java
@@ -12,7 +12,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import static javax.persistence.CascadeType.*;
 
 /**
  * @author Yarullin Renat
@@ -26,7 +31,7 @@ import java.util.*;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_generator")
-    @SequenceGenerator(name="user_generator", sequenceName = "user_seq", allocationSize=50)
+    @SequenceGenerator(name = "user_generator", sequenceName = "user_seq", allocationSize = 50)
     @Column(unique = true)
     private Long id;
     @Column(unique = true)
@@ -34,16 +39,17 @@ public class User implements UserDetails {
     private String passwordHash;
     private String name;
     private String surname;
+    private String messenger;
     private String middleName;
     private String interests;
     private Integer reputation;
     private String email;
-    @ElementCollection
-    private List<String> roles;
     private String groupp;
 
+    @ManyToMany(cascade = {PERSIST, MERGE, DETACH, REFRESH, PERSIST})
+    private Set<ProjectRole> roles;
     @ManyToMany
-    @Cascade({ org.hibernate.annotations.CascadeType.PERSIST,
+    @Cascade({org.hibernate.annotations.CascadeType.PERSIST,
             org.hibernate.annotations.CascadeType.MERGE,
             org.hibernate.annotations.CascadeType.DETACH,
             org.hibernate.annotations.CascadeType.REFRESH,
@@ -53,27 +59,27 @@ public class User implements UserDetails {
             name = "user_skills",
             joinColumns = @JoinColumn(name = "user_login"),
             inverseJoinColumns = @JoinColumn(name = "tag_id"))
-    private Set<Tags> skills=new HashSet<>();
+    private Set<Tags> skills = new HashSet<>();
 
     @ElementCollection(targetClass = AccessRole.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<AccessRole> accessRoles;
 
-    public User(String login, String passwordHash, String name, String surname, String email, List<String> roles, String interests, String groupp, Set<Tags> skills, Set<AccessRole> accessRoles) {
+    public User(String login, String passwordHash, String name, String surname, String email, String interests, String groupp, Set<Tags> skills, Set<AccessRole> accessRoles) {
         this.login = login;
         this.passwordHash = passwordHash;
         this.name = name;
         this.surname = surname;
         this.email = email;
-        this.roles = roles;
         this.interests = interests;
         this.groupp = groupp;
-        this.skills=skills;
+        this.skills = skills;
 
         this.accessRoles = accessRoles;
 
         this.reputation = 100;
+        this.roles = new HashSet<>();
     }
 
     @Override
