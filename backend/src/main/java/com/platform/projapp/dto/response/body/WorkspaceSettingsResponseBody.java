c45772bc0d95dee7a51df63403d67298751a6005
PAINKILLER-F <4realmadrid14@gmail.com>
2021-11-25 16:10:26 +0500

    Workspace settings added

---
+++ backend/src/main/java/com/platform/projapp/dto/response/body/WorkspaceSettingsResponseBody.java
@@ -0,0 +1,25 @@
+package com.platform.projapp.dto.response.body;
+
+import com.platform.projapp.enumarate.WorkspaceRole;
+import com.platform.projapp.model.Workspace;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class WorkspaceSettingsResponseBody {
+    private String title;
+    private LocalDate startDate;
+    private Integer sprintLenght;
+    private Integer sprintsCount;
+
+    public static WorkspaceSettingsResponseBody fromWorkspace(Workspace workspace) {
+        return new WorkspaceSettingsResponseBody(workspace.getName(),
+                workspace.getZeroSprintDate(),
+                workspace.getFrequencyOfSprints(),
+                workspace.getSprintCount());
+    }
+
+}
