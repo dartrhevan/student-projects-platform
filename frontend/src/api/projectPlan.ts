b20de8ea54a58977189cef18f611fcf201657687
r_svv <svv@renue.ru>
2021-12-04 22:00:50 +0500

    presentation upload

--- frontend/src/api/projectPlan.ts
+++ frontend/src/api/projectPlan.ts
@@ -1,8 +1,5 @@
-import GenericResponse from "../model/dto/GenericResponse";
-import Sprint, {ProjectPlan, ResultComment} from "../model/Sprint";
-import CommonResponse from "../model/dto/CommonResponse";
-import {StorageKeys} from "../utils/StorageKeys";
-import {getDefaultDownloadHandler, getDefaultUploadHandler, toDateString} from "../utils/utils";
+import Sprint from "../model/Sprint";
+import {getDefaultDownloadHandler, getDefaultUploadHandler, toBase64, toDateString} from "../utils/utils";
 import {getTokenHeader} from "../store/state/LoginState";
 
 /**
@@ -15,10 +12,6 @@ export function getProjectPlan(projectId: string) {//TODO: implement
     return fetch(`/api/sprints?projectId=${projectId}`, {
         headers: getTokenHeader()
     }).then(getDefaultDownloadHandler());
-    // return new Promise<GenericResponse<ProjectPlan>>((res, rej) => res(
-    //     new GenericResponse(new ProjectPlan(
-    //         [new Sprint('1', new Date(), new Date(2021, 12, 19), 'To start',
-    //             'http://ya.ru', [new ResultComment('1', 'WERTYUI', 'YYY')])], 'My project'))));
 }
 
 /**
@@ -30,7 +23,6 @@ export function removeSprint(sprintId: string) {
         method: 'DELETE',
         headers: getTokenHeader()
     }).then(getDefaultUploadHandler());
-    // return new Promise<CommonResponse>(resolve => resolve(new CommonResponse())); //TODO: implement
 }
 
 /**
@@ -56,7 +48,6 @@ export function addSprint(projectId: string, orderNum: string, sprint: Sprint) {
             projectId
         })
     }).then(getDefaultDownloadHandler());
-    // return new Promise<GenericResponse<string>>(resolve => resolve(new GenericResponse(""))); //TODO: implement
 }
 
 /**
@@ -64,40 +55,25 @@ export function addSprint(projectId: string, orderNum: string, sprint: Sprint) {
  *
  * @param sprint
  */
-export function updateSprint(sprint: Sprint) {
-    // console.log(sprint);
-    // return new Promise<CommonResponse>(resolve => resolve(new CommonResponse()))
+export function updateSprint(sprint: Sprint, presentation?: File) {
+    function sendUpdate(pr?: string) {
+        return fetch(`/api/sprints`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                ...getTokenHeader()
+            },
+            body: JSON.stringify({
+                sprintId: sprint.id,
+                // number: sprint.
+                goals: sprint.goals,
+                startDate: toDateString(new Date(sprint.startDate)),
+                endDate: toDateString(new Date(sprint.endDate)),
+                presentation: pr
+            })
 
-    return fetch(`/api/sprints`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            ...getTokenHeader()
-        },
-        body: JSON.stringify({
-            sprintId: sprint.id,
-            // number: sprint.
-            goals: sprint.goals,
-            startDate: toDateString(new Date(sprint.startDate)),
-            endDate: toDateString(new Date(sprint.endDate))
-        })
-
-    }).then(getDefaultUploadHandler());
+        }).then(getDefaultUploadHandler());
+    }
+    if (presentation) return toBase64(presentation).catch(console.log).then(pr => sendUpdate(pr as string));
+    return sendUpdate();
 }
-
-/**
- *
- * @param projectId
- * @param sprintId
- * @param presentation
- * @return presentation url
- */
-export function uploadPresentation(projectId: string, sprintId: string, presentation: File) {//TODO: implement
-    console.log(presentation.name);
-    return new Promise<GenericResponse<string>>(resolve => resolve(new GenericResponse("")));
-}
-
-
-// export function dropPlan(workspaceId: string, projectId: string) {
-//     return new Promise<CommonResponse>(resolve => resolve(new CommonResponse()));
-// }
